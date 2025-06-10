5ae9793a42f5e976ac09018be05d2bdc2f82e696
MohinSayyad18 <127471329+MohinSayyad18@users.noreply.github.com>
2025-06-10 17:24:39 +0530

    updated code

--- DMS_goa/admin_web/serializers.py
+++ DMS_goa/admin_web/serializers.py
@@ -436,12 +436,8 @@ class dispatchsopserializer(serializers.ModelSerializer):
     alert_id = serializers.CharField(source='alert_id.disaster_name', read_only=True)
     class Meta:
         model = DMS_Incident
-        fields = ['inc_id','incident_id','alert_id','disaster_type','inc_added_by','inc_added_date','disaster_name','inc_type','mode']
+        fields = ['inc_id','incident_id','alert_id','disaster_type','inc_added_by','inc_added_date','disaster_name','inc_type','mode','alert_type']
         
-    def to_representation(self, instance):
-        rep = super().to_representation(instance)
-        rep['alert_type'] = rep.pop('inc_type')  
-        return rep
 
 
 class incident_get_serializer(serializers.ModelSerializer):
