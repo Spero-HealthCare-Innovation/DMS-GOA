80269e4bb6fae5ac8b9aea0a83aeec09c456970e
vinayak9775 <vinayak@sperohealthcare.in>
2025-08-11 18:07:48 +0530

    updated

--- DMS_goa/admin_web/views.py
+++ DMS_goa/admin_web/views.py
@@ -437,6 +437,7 @@ class UserLoginView(APIView):
  
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
+            # user_username = serializer.data.get('user_username') 
             user_username = serializer.data.get('user_username')
             password = serializer.data.get('password')
             print("=========", user_username, password)
