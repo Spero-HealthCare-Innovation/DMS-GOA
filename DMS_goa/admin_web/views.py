2b00aa1751e7e63acd2f5fce12ecb2f3a77065b0
vinayak9775 <vinayak@sperohealthcare.in>
2025-08-11 18:11:27 +0530

    hupdated

--- DMS_goa/admin_web/views.py
+++ DMS_goa/admin_web/views.py
@@ -437,11 +437,8 @@ class UserLoginView(APIView):
  
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            user_username = serializer.data.get('emp_username')
-=======
             # user_username = serializer.data.get('user_username') 
             user_username = serializer.data.get('user_username')
->>>>>>> Testing
             password = serializer.data.get('password')
             print("=========", user_username, password)
             user = authenticate(user_username=user_username, password=password)
