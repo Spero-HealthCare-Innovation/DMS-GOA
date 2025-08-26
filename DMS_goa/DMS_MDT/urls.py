e6d8c67878dd89cffaa814134d230c297b7544e1
SandipShimpi001 <sandip.speroinfosystems@gmail.com>
2025-08-26 12:26:20 +0530

    change add device api

--- DMS_goa/DMS_MDT/urls.py
+++ DMS_goa/DMS_MDT/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('VehicleLogin/', VehicleLogin.as_view()),
     path('VehicleLogout/', VehicleLogout.as_view()),
     path('employee_list/', employee_list.as_view()),
-    path('add_device/', add_device.as_view()),
+    path('api/device/', add_device.as_view()),
     path('Vehical_department_wise/',Vehical_department_wise.as_view()),
     path('vehical_base_loc/',get_base_location_vehicle.as_view()),
     path('vehical/',get_vehicle.as_view()),
